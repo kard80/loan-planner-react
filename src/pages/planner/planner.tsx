e8e225161d7d3b7e1e9eef05e6c91e43916f8327
kard sahaphong <kard.sp2Wgmail.com>
2022-01-20 00:25:30 +0700

    planner new features: add and delete pane tabs

--- src/pages/planner/planner.tsx
+++ src/pages/planner/planner.tsx
@@ -1,4 +1,4 @@
-import { Button, Pane, Tab, Table, Tablist, Text, TextInputField, Tooltip } from 'evergreen-ui';
+import { AddIcon, Button, DeleteIcon, Dialog, EditIcon, Pane, Tab, Table, Tablist, Text, TextInputField } from 'evergreen-ui';
 import { useState, ChangeEvent, useEffect } from 'react';
 import { FetchData } from '../../services/http';
 import { PlannerRequest, PlannerResponse, MonthDetail } from '../../types/serviceRequest';
@@ -16,8 +16,10 @@ type props = {
 }
 
 const defaultProfileList: PlannerRequest[] = [{loanAmount: '', interestRate: '', installment: '', label: 'Pane 1'}];
+const profileLimit = 3;
 
 function Planner({ dispatch }: props) {
+    const [isShowDialog, setIsShowDialog] = useState(false);
     const [profileList, setProfileList] = useState<PlannerRequest[]>(defaultProfileList);
     const [output, setOutput] = useState<PlannerResponse>({ months: [] as MonthDetail[] });
     const [selectedTab, setSelectedTab] = useState(0);
@@ -29,8 +31,7 @@ function Planner({ dispatch }: props) {
         if (user) {
             fetchInputList();
         } else {
-            setSelectedTab(0);
-            setProfileList(defaultProfileList);
+            setProfiles(defaultProfileList, 0)
         }
     }, [user])
 
@@ -39,7 +40,36 @@ function Planner({ dispatch }: props) {
         setProfileList(inputPlannerList);
     }
 
-    const handleOnSubmit = async () => {
+    const setProfiles = (profiles: PlannerRequest[], tab: number): void => {
+        setSelectedTab(tab);
+        setProfileList(profiles);
+    }
+
+    const onAddClicked = () => {
+        const arr : PlannerRequest[] = [...profileList,  { ...defaultProfileList[0], label: `New Pane`}];
+        console.log(arr);
+        setProfiles(arr, arr.length - 1);
+        console.log(profileList);
+    }
+
+    const onConfirmDeleteProfile = (close: () => void) => {
+        const arr = [...profileList];
+        arr.splice(selectedTab, 1);
+        setProfiles(arr, 0);
+        close();
+    }
+
+    const onInputChange = (key: keyof PlannerRequest, value: string) => {
+        const profiles = profileList.map((item, index) => {
+            if (index === selectedTab) {
+                item[key] = value;
+            }
+            return item;
+        })
+        setProfileList(profiles);
+    }
+
+    const onSubmit = async () => {
         showSpinner(true);
         try {
             const fetch = await FetchData(`/plannerCalculation?loanAmount=${profileList[selectedTab].loanAmount}&interestRate=${profileList[selectedTab].interestRate}&installment=${profileList[selectedTab].installment}`);
@@ -60,23 +90,45 @@ function Planner({ dispatch }: props) {
     return(
         <Pane className='planner-container' display='flex' flexDirection='column' alignItems='center'>
             <Pane className='input-part'>
-                <Tablist marginBottom='10px'>
-                    {!!user && profileList.map((profile, index) => {
-                        return(
-                            <Tab key={index} isSelected={index === selectedTab} onSelect={() => onTabSelected(index)}>{profile.label}</Tab>
-                        )
-                    })}
-                </Tablist>
+                <Pane display='flex' justifyContent='space-between'>
+                    <Tablist marginBottom='10px'>
+                        {!!user && profileList.map((profile, index) => {
+                            return(
+                                <Tab key={index} isSelected={index === selectedTab} onSelect={() => onTabSelected(index)}>
+                                    {profile.label}
+                                </Tab>
+                            )
+                        })}
+                        { !!user && profileList.length < profileLimit && <Tab><AddIcon size={12} color='info' onClick={() => onAddClicked()}></AddIcon></Tab>}
+                    </Tablist>
+                    { !!user &&
+                        <Pane minWidth='20px' display='flex' justifyContent='flex-end' gap='8px'>
+                            <EditIcon cursor='pointer' />
+                            { profileList.length > 1 && <DeleteIcon color='danger' cursor='pointer' onClick={() => setIsShowDialog(true)} />}
+                        </Pane>}
+                    <Dialog
+                        isShown={isShowDialog}
+                        title='Warning'
+                        intent='danger'
+                        onConfirm={(close: () => void) => onConfirmDeleteProfile(close)}
+                        onCloseComplete={() => setIsShowDialog(false)}>
+                            <Text>คุณต้องการลบ</Text>
+                            {profileList[selectedTab].label
+                                ? <Text size={500} fontWeight='bold'> {profileList[selectedTab].label} </Text>
+                                : <Text>ข้อมูล</Text>}
+                            <Text>หรือไม่?</Text>
+                    </Dialog>
+                </Pane>
                 <Pane className='planner-part'>
-                    <TextInputField label='วงเงินกู้' type='number' step={100000} min='0' required value={profileList[selectedTab].loanAmount} onChange={(e: ChangeEvent<HTMLInputElement>) => setProfileList([{...profileList[selectedTab], loanAmount: e.target.value}])} />
+                    <TextInputField label='วงเงินกู้' type='number' step={100000} min='0' required value={profileList[selectedTab].loanAmount} onChange={(e: ChangeEvent<HTMLInputElement>) => onInputChange('loanAmount', e.target.value)} />
                     {/* <TextInputField label='ระยะเวลาผ่อนชำระ(ปี)' type='number' min='1' required /> */}
-                    <TextInputField label='อัตราดอกเบี้ยสามปีแรก' type='number' min='0' required value={profileList[selectedTab].interestRate} onChange={(e: ChangeEvent<HTMLInputElement>) => setProfileList([{...profileList[selectedTab], interestRate: e.target.value}])} />
-                    <TextInputField label='ผ่อนชำระงวดละ(บาท)' type='number' min='0' step={1000} required value={profileList[selectedTab].installment} onChange={(e: ChangeEvent<HTMLInputElement>) => setProfileList([{...profileList[selectedTab], installment: e.target.value}])} />
+                    <TextInputField label='อัตราดอกเบี้ยสามปีแรก' type='number' min='0' required value={profileList[selectedTab].interestRate} onChange={(e: ChangeEvent<HTMLInputElement>) => onInputChange('interestRate', e.target.value)} />
+                    <TextInputField label='ผ่อนชำระงวดละ(บาท)' type='number' min='0' step={1000} required value={profileList[selectedTab].installment} onChange={(e: ChangeEvent<HTMLInputElement>) => onInputChange('installment', e.target.value)} />
                     {/* <TextInputField label='วันที่ชำระงวดแรก' type='date' /> */}
                     {/* <TextInputField label='อัตราดอกเบี้ยสามปีหลัง' type='number' min='0' /> */}
                 </Pane>
                 <Pane display='flex' justifyContent='end' alignItems='center' padding='10px'>
-                    <Button appearance='primary' maxHeight='40px' onClick={handleOnSubmit} >คำนวณ</Button>
+                    <Button appearance='primary' maxHeight='40px' onClick={onSubmit} >คำนวณ</Button>
                 </Pane>
             </Pane>
             <Pane className='output-part' display={output.months.length ? 'block' : 'none'}>
